290d7c68d05d80ea5b75f118f9abf8cd204c484c
Alex Lovy <Alex.lovy@gmail.com>
2023-05-23 00:38:53 +0300

    add task 27

---
+++ task_27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int Sum(string number)
+{
+
+char[] ch = number.ToCharArray();
+
+int sum = 0;
+
+int a = 0;
+
+for (int i = 0; i <= number.Length - 1; i++)
+{
+
+   a = int.Parse(Convert.ToString(ch[i]));
+
+   sum = sum + a;
+
+}
+    return sum;
+}
+
+System.Console.WriteLine("Введите число: ");
+string num = (Console.ReadLine());
+int sum = Sum(num);
+System.Console.WriteLine($"Сумма цифр числа {num} = {sum}");
